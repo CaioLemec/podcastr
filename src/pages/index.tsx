65071e9bfc17963897509472e2f40a15362483c1
Caio Lemec <caio.lemec@gmail.com>
2021-04-22 16:59:50 -0300

    player context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 import styles from './home.module.scss'
 
 type Episode = {
@@ -12,7 +14,7 @@ type Episode = {
   title: string,
   thumbnail: string,
   members: string,
-  duration: string,
+  duration: number,
   durationAsString: string,
   publishedAt: string,
   url: string,
@@ -24,6 +26,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+  
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -49,7 +53,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="play podcast"/>
                 </button>
             </li>
