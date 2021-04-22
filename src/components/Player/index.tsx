3ea56f2b762393aeaf4bd262e8b060d38ad81883
Caio Lemec <caio.lemec@gmail.com>
2021-04-22 20:44:05 -0300

    adding styles at player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -51,19 +51,19 @@ export function Player() {
                 </div>
 
                 <div className={styles.buttons}>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar"/>
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-previous.svg" alt="Tocar anterior"/>
                     </button>
-                    <button type="button" className={styles.playButton}>
+                    <button type="button" className={styles.playButton} disabled={!episode}>
                         <img src="/play.svg" alt="Tocar"/>
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar pŕoximo"/>
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/repeat.svg" alt="repetir"/>
                     </button>
                 </div>
