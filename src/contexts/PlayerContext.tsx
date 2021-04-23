64ddf1613a0d512f32bb4cb27c393463a75cd908
Caio Lemec <caio.lemec@gmail.com>
2021-04-23 11:01:37 -0300

    refatoring contexts

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,58 @@
+import { createContext, useState, ReactNode } from 'react';
+
+type Episode = {
+    title: string;
+    thumbnail: string;
+    members: string;
+    duration: number;
+    url: string;
+};
+
+type PlayerContextData = {
+    episodeList: Episode[];
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    togglePlay: () => void;
+    setPlayinState: (state: boolean) => void;
+    play: (episode: Episode) => void;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData)
+
+type PlayerContextProviderProps = {
+    children: ReactNode,
+}
+
+export function PlayerContextProvider ( { children }: PlayerContextProviderProps ) {
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false);
+  
+    function play(episode: Episode) {
+      setEpisodeList([episode])
+      setCurrentEpisodeIndex(0);
+      setIsPlaying(true);
+    }
+  
+    function togglePlay() {
+      setIsPlaying(!isPlaying);
+    }
+  
+    function setPlayinState(state: boolean) {
+      setIsPlaying(state);
+    }
+  
+    return (
+      <PlayerContext.Provider value={{
+                episodeList, 
+                currentEpisodeIndex, 
+                play, isPlaying, 
+                togglePlay, 
+                setPlayinState
+            }}
+        >
+      {children}
+      </PlayerContext.Provider>
+      )
+}
+
